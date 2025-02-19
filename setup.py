b75cd9be5e3bfcd77c856f9bc17149a182354af2
Viet Trinh <viettrinh271291@gmail.com>
2025-02-19 23:18:24 +0700

    Update install_requires and add a nltk's tagger to support English

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ def install_nltk_data():
     import nltk
     nltk.download('wordnet')
     nltk.download('averaged_perceptron_tagger')
+    nltk.download('averaged_perceptron_tagger_eng')
 
 class PostDevelopCommand(develop):
     def run(self):
@@ -20,20 +21,20 @@ class PostInstallCommand(install):
 setup(
     name='wisecreator',
     version='1.0.0',
-    python_requires='>=3.6',
+    python_requires='>=3.13',
     author='Timofey Milovanov',
     packages=['wisecreator'],
     package_data={
         'wisecreator':['data/*', 'third_party/*']
     },
     install_requires=[
-        'nltk==3.4.5',
+        'nltk==3.9.1',
         'cursor==1.3.4',
-        'six==1.12.0',
+        'six==1.17.0',
         'dataclasses',
     ],
     setup_requires=[
-        'nltk==3.4.5',
+        'nltk==3.9.1',
     ],
     cmdclass={
       'install': PostInstallCommand,
